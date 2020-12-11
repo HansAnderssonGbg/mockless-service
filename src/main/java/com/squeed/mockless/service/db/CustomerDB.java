9db333f56c672952f1d90097e2587570d012abb6
Max Gabrielsson <max.gabrielsson@gmail.com>
2020-12-11 15:38:23 +0100

    Flyway migrationscripts invoked
    Signed-off-by: Max Gabrielsson <max.gabrielsson@gmail.com>

--- src/main/java/com/squeed/mockless/service/db/CustomerDB.java
+++ src/main/java/com/squeed/mockless/service/db/CustomerDB.java
@@ -1,11 +1,16 @@
 package com.squeed.mockless.service.db;
 
 import io.quarkus.hibernate.orm.panache.PanacheEntity;;
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
 import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
 @Entity
@@ -13,6 +18,7 @@ import javax.persistence.Table;
 @Getter
 @Setter
 public class CustomerDB extends PanacheEntity {
+
 
     @Column(name = "first_name")
     String firstName;
